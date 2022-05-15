80e1801d6c35b58052d7335d82da4de4b1763d53
andrew-causelof <akozlov@rocketmail.com>
2022-05-15 22:27:08 +0300

    excluded images

--- sync.sh
+++ sync.sh
@@ -1,1 +1,2 @@
 rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/organ-music.causelof.ru/public_html/* ./
+rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/organ-music.causelof.ru/public_html/.[^.]* ./
