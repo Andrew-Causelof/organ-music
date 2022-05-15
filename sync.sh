a5d5e5e217c63129d71be6fe3864d356b55e6447
andrew-causelof <akozlov@rocketmail.com>
2022-05-15 22:30:35 +0300

    excluded music folder

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/organ-music.causelof.ru/public_html/* ./
 rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/organ-music.causelof.ru/public_html/.[^.]* ./
+
